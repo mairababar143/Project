fc77ead23114d8f25be6387ee3f8591deebb1e98
mairababar143 <52742990+mairababar143@users.noreply.github.com>
2019-12-22 23:16:26 +0500

    Add files via upload

---
+++ WindowsFormsAppBluetoothProject/WindowsFormsAppBluetoothProject/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace WindowsFormsAppBluetoothProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBoxConnection = new System.Windows.Forms.RichTextBox();
+            this.buttonServer = new System.Windows.Forms.Button();
+            this.listBoxItem = new System.Windows.Forms.ListBox();
+            this.textBoxMessage = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.buttonClient = new System.Windows.Forms.Button();
+            this.buttonClick = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBoxConnection
+            // 
+            this.richTextBoxConnection.Location = new System.Drawing.Point(23, 82);
+            this.richTextBoxConnection.Name = "richTextBoxConnection";
+            this.richTextBoxConnection.Size = new System.Drawing.Size(648, 384);
+            this.richTextBoxConnection.TabIndex = 0;
+            this.richTextBoxConnection.Text = "";
+            this.richTextBoxConnection.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // buttonServer
+            // 
+            this.buttonServer.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonServer.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonServer.ImageKey = "search.jpg";
+            this.buttonServer.ImageList = this.imageList1;
+            this.buttonServer.Location = new System.Drawing.Point(745, 121);
+            this.buttonServer.Name = "buttonServer";
+            this.buttonServer.Size = new System.Drawing.Size(313, 54);
+            this.buttonServer.TabIndex = 2;
+            this.buttonServer.Text = "Available Connection";
+            this.buttonServer.UseVisualStyleBackColor = true;
+            this.buttonServer.Click += new System.EventHandler(this.buttonServer_Click);
+            // 
+            // listBoxItem
+            // 
+            this.listBoxItem.FormattingEnabled = true;
+            this.listBoxItem.ItemHeight = 20;
+            this.listBoxItem.Location = new System.Drawing.Point(745, 222);
+            this.listBoxItem.Name = "listBoxItem";
+            this.listBoxItem.Size = new System.Drawing.Size(313, 244);
+            this.listBoxItem.TabIndex = 3;
+            this.listBoxItem.SelectedIndexChanged += new System.EventHandler(this.listBoxItem_SelectedIndexChanged);
+            this.listBoxItem.DoubleClick += new System.EventHandler(this.listBoxItem_DoubleClick);
+            // 
+            // textBoxMessage
+            // 
+            this.textBoxMessage.Location = new System.Drawing.Point(23, 511);
+            this.textBoxMessage.Name = "textBoxMessage";
+            this.textBoxMessage.Size = new System.Drawing.Size(648, 26);
+            this.textBoxMessage.TabIndex = 4;
+            this.textBoxMessage.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxMessage_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(181, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(303, 32);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Bluetooth Messenger";
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "incoming.png");
+            this.imageList1.Images.SetKeyName(1, "search.jpg");
+            // 
+            // buttonClient
+            // 
+            this.buttonClient.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonClient.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonClient.ImageIndex = 0;
+            this.buttonClient.ImageList = this.imageList1;
+            this.buttonClient.Location = new System.Drawing.Point(745, 43);
+            this.buttonClient.Name = "buttonClient";
+            this.buttonClient.Size = new System.Drawing.Size(313, 57);
+            this.buttonClient.TabIndex = 1;
+            this.buttonClient.Text = "Incomming Request";
+            this.buttonClient.UseVisualStyleBackColor = true;
+            this.buttonClient.Click += new System.EventHandler(this.buttonClient_Click);
+            // 
+            // buttonClick
+            // 
+            this.buttonClick.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonClick.Location = new System.Drawing.Point(817, 500);
+            this.buttonClick.Name = "buttonClick";
+            this.buttonClick.Size = new System.Drawing.Size(165, 49);
+            this.buttonClick.TabIndex = 6;
+            this.buttonClick.Text = "Send";
+            this.buttonClick.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1130, 625);
+            this.Controls.Add(this.buttonClick);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxMessage);
+            this.Controls.Add(this.listBoxItem);
+            this.Controls.Add(this.buttonServer);
+            this.Controls.Add(this.buttonClient);
+            this.Controls.Add(this.richTextBoxConnection);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Bluetooth Messemger";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBoxConnection;
+        private System.Windows.Forms.Button buttonClient;
+        private System.Windows.Forms.Button buttonServer;
+        private System.Windows.Forms.ListBox listBoxItem;
+        private System.Windows.Forms.TextBox textBoxMessage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Button buttonClick;
+    }
+}
+
---
+++ WindowsFormsAppBluetoothProject/WindowsFormsAppBluetoothProject/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using InTheHand;
+using InTheHand.Net.Bluetooth;
+using InTheHand.Net.Ports;
+using InTheHand.Net.Sockets;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppBluetoothProject
+{
+    public partial class Form1 : Form
+    {
+        List<string> items;
+        public Form1()
+        {
+            InitializeComponent();
+            items = new List<string>();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonServer_Click(object sender, EventArgs e)
+        {
+            startScan();
+        }
+
+        private void startScan()
+        {
+            listBoxItem.DataSource = null;  //before every scan
+            listBoxItem.Items.Clear();
+            items.Clear();
+            Thread bluetoothScanThread = new Thread(new ThreadStart(scan));
+            bluetoothScanThread.Start();
+        }
+
+        BluetoothDeviceInfo[] devices;
+        public void scan()
+        {
+            updatedUI("Start Scan....");
+            BluetoothClient client = new BluetoothClient();
+            devices = client.DiscoverDevicesInRange();
+            updatedUI("Scan Complete");
+            updatedUI(devices.Length.ToString() + "  Devise Discovered");
+            foreach (BluetoothDeviceInfo d in devices)
+            {
+                items.Add(d.DeviceName);
+            }
+            updateDeviceList();
+        }
+
+        public void updateDeviceList()
+        {
+            Func<int> del = delegate ()
+            {
+                listBoxItem.DataSource = items;
+                return 0;
+            };
+            Invoke(del);
+
+        }
+
+        private void buttonClient_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void updatedUI(string message)
+        {
+            Func<int> del = delegate ()
+            {
+                richTextBoxConnection.AppendText(message + System.Environment.NewLine);
+                return 0;
+            };
+            Invoke(del);
+        }
+
+
+        private void listBoxItem_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        
+
+        private void textBoxMessage_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void listBoxItem_DoubleClick(object sender, EventArgs e)
+        {
+        }
+    }
+}
